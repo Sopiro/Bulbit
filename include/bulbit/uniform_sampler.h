956204dc4ca2e64f5123d9eb783b5ecfa857a8cd
sopiro <sopiro.ldh@gmail.com>
2023-10-31 07:10:00 +0900

    Create uniform sampler

---
+++ include/bulbit/uniform_sampler.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include "random.h"
+#include "sampler.h"
+
+namespace bulbit
+{
+
+class UniformSampler : public Sampler
+{
+public:
+    UniformSampler(int32 samples_per_pixel, int32 seed);
+    virtual ~UniformSampler() = default;
+
+    virtual Float Next1D() override;
+    virtual Point2 Next2D() override;
+
+    virtual std::unique_ptr<Sampler> Clone(int32 seed) override;
+
+private:
+    RNG rng;
+};
+
+inline UniformSampler::UniformSampler(int32 samples_per_pixel, int32 seed)
+    : Sampler(samples_per_pixel)
+{
+    rng.Seed(seed);
+}
+
+inline Float UniformSampler::Next1D()
+{
+    return rng.NextFloat();
+}
+
+inline Point2 UniformSampler::Next2D()
+{
+    return { rng.NextFloat(), rng.NextFloat() };
+}
+
+inline std::unique_ptr<Sampler> UniformSampler::Clone(int32 seed)
+{
+    return std::make_unique<UniformSampler>(samples_per_pixel, seed);
+}
+
+} // namespace bulbit
